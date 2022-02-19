b2a032c3de7ed76fc5f3272bac648a2b58edaf65
Anthony <anthonymarques@outlook.com>
2022-02-19 12:11:17 +0200

    added components did some styling for the tiles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { GameTileComponent } from './components/game-tile/game-tile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    GameTileComponent
   ],
   imports: [
     BrowserModule,
